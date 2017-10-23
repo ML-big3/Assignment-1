4435a65deff8fb6e84d162c482fc49a983178d9e
Amit Prasad <amitprasad53121@gmail.com>
2017-10-24 00:40:28 +0100

    Evaluation class defined for commonizing the code

--- Task2/DecisionTree_Skin.py
+++ Task2/DecisionTree_Skin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 22 02:38:29 2017
+Created on Mon Oct 23 22:24:47 2017
 
 """
 
@@ -10,6 +10,7 @@ import pandas as pd
 
 import config
 import evaluation
+
 
 dataset = pd.read_csv(config.SKIN_DATA_SET, sep='\t')
 dataset = dataset.reindex(np.random.permutation(dataset.index))
@@ -21,17 +22,12 @@ y = y[:10000]
 # data cleaning
 y = [yi -1 for yi in y]
 
-# feature Scalaing
 
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X = sc_X.fit_transform(X)
+# Decision Tree
 
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy')
 
-# KNN
-
-from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors= 20, metric = 'minkowski', p=2)
 
 # Evaluating the performance using 10 fold cross validation
 evaluationMetric = evaluation.EvaluationMetrics(classifier, X, y, 10, 7)
---
+++ Task2/evaluation.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 23:09:37 2017
+
+Evaluation Metrics
+"""
+
+from sklearn import model_selection
+from sklearn.metrics import make_scorer
+from sklearn.metrics import log_loss
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+
+class EvaluationMetrics:
+    
+    def __init__(self, classifier, X, y, splits, seed):
+        self.classifier = classifier
+        self.X = X
+        self.y = y
+        self.kfold = model_selection.KFold(n_splits=10, random_state=seed)
+        
+    def crossValidateForAccuracy(self):
+        results = model_selection.cross_val_score(self.classifier, self.X, self.y, cv=self.kfold, scoring='accuracy')
+        print("Accuracy: %.3f (%.3f)") % (results.mean(), results.std())
+
+    def crossValidateLogLoss(self):
+        scorer = make_scorer(log_loss)
+        results = model_selection.cross_val_score(self.classifier, self.X, self.y, cv=self.kfold, scoring=scorer)
+        print("LogLoss: %.3f (%.3f)") % (results.mean(), results.std())
+
+    def crossValidateAucRoc(self):
+        scorer = make_scorer(roc_auc_score)
+        results = model_selection.cross_val_score(self.classifier, self.X, self.y, cv=self.kfold, scoring=scorer)
+        print("Area Under ROC Curve: %.3f (%.3f)") % (results.mean(), results.std())        
+
+    def crossValidatePrecisionScore(self):
+        scorer = make_scorer(precision_score)
+        results = model_selection.cross_val_score(self.classifier, self.X, self.y, cv=self.kfold, scoring=scorer)
+        print("Precision Score: %.3f (%.3f)") % (results.mean(), results.std())
+    
+    def crossValidateConfusionMatrix(self):
+        def tp(y_true, y_pred): return confusion_matrix(y_true, y_pred)[0, 0]
+        def tn(y_true, y_pred): return confusion_matrix(y_true, y_pred)[0, 0]
+        def fp(y_true, y_pred): return confusion_matrix(y_true, y_pred)[1, 0]
+        def fn(y_true, y_pred): return confusion_matrix(y_true, y_pred)[0, 1]
+        cm = {'tp' : make_scorer(tp), 'tn' : make_scorer(tn),
+                   'fp' : make_scorer(fp), 'fn' : make_scorer(fn)}
+        results = model_selection.cross_validate(self.classifier, self.X, self.y, cv=self.kfold, scoring=cm)
+        print("Confusion Maxtrix: ", results)
+        
