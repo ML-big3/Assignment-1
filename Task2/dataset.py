4c9a7b725347f7877e33179702912ec4792299a3
Amit Prasad <amitprasad53121@gmail.com>
2017-10-24 19:16:18 +0100

    Added implementation for using the SUSY dataset

--- Task2/dataset.py
+++ Task2/dataset.py
@@ -3,8 +3,8 @@
 import numpy as np
 import pandas as pd
 
-def skinNoSkinDataset(pathToDataset):
-    dataset = pd.read_csv(pathToDataset, sep='\t')
+def skinNoSkinDataset(path_data_set):
+    dataset = pd.read_csv(path_data_set, sep='\t')
     dataset = dataset.reindex(np.random.permutation(dataset.index))
     X = dataset.iloc[:, 0:3].values
     X = X[:10000]
@@ -13,5 +13,17 @@ def skinNoSkinDataset(pathToDataset):
     
     # data cleaning
     y = [yi -1 for yi in y]
+    
+    return (X, y)
+
+def susyDataset(path_data_set):
+    dataset = pd.read_csv(path_data_set, sep=',')
+    X = dataset.iloc[:, 1:].values
+    X = X[:10000]
+    y = dataset.iloc[:,0].values
+    y = y[:10000]
+    
+    # data cleaning
+    y = [int(yi) for yi in y]
     
     return (X, y)
--- Task2/init.py
+++ Task2/init.py
@@ -3,11 +3,19 @@
 import config
 import dataset
 import DecisionTree
+import LogisticRegression
+import SVM
 import KNN
 
 
-X, y = dataset.skinNoSkinDataset(config.SKIN_DATA_SET)
+#X, y = dataset.skinNoSkinDataset(config.SKIN_DATA_SET)
+X, y = dataset.susyDataset(config.SUSY_DATA_SET)
 
+
+LogisticRegression.logisticRegressionClassifier(X, y)
+SVM.svmClassifier(X, y)
 DecisionTree.decisionTreeClassifier(X, y)
 KNN.knnClassifier(X,y) 
+
+
 
