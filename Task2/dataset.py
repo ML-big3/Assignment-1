d3919601792d73ffd917dd7f749349c1b70c6e3b
Amit Umashankar Prasad <prasada@ITSERV.SCSS.TCD.IE>
2017-10-25 21:00:05 +0100

    SUSY dataset minimized to 50000 values. Results added

--- Task2/dataset.py
+++ Task2/dataset.py
@@ -19,9 +19,9 @@ def skinNoSkinDataset(path_data_set):
 def susyDataset(path_data_set):
     dataset = pd.read_csv(path_data_set, sep=',')
     X = dataset.iloc[:, 1:].values
-    #X = X[:10000]
+    X = X[:50000]
     y = dataset.iloc[:,0].values
-    #y = y[:10000]
+    y = y[:50000]
     
     # data cleaning
     y = [int(yi) for yi in y]
